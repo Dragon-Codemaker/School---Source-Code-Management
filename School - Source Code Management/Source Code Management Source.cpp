2718d0f6bed58adddcadde8493233471c672a992
alexs <alexs@BLORBO>
2024-10-25 22:26:44 -0400

    Add project files.

---
+++ School - Source Code Management/Source Code Management Source.cpp
@@ -0,0 +1,17 @@
+//Name: Alexandra Smith
+//Class: CIS 1202.501
+//Project Name: Source Code Management
+//Project Description: This program is designed to print the text "Hello World!" to the console, for the purposes of having a program to learn source code management with
+//Date: 10/25/2024
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World! :3" << endl;
+
+	system("pause");
+	return 0;
+}
